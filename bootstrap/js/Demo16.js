4ade4340692b1052a940b9b91e03426310341187
Terry Xu (Wicresoft North America) <v-tex@microsoft.com>
2017-11-24 17:20:51 +0800

    2017/11/24

---
+++ bootstrap/js/Demo16.js
@@ -0,0 +1,21 @@
+$(function () {
+    var acc = document.getElementsByClassName("panel-heading");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function () {
+            //this.classList.toggle("active");
+            this.firstChild.nextElementSibling.firstElementChild.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        }
+    }
+
+    $('.register').on('click',function(){
+        window.location.href ='D:/GitProject/JavaScript/Demo13.html';
+    });
+})
